f4c3a6c0f09c86f6ab6de34793e45a02adb03513
Barry <barry2014158@gmail.com>
2019-06-19 18:56:33 +0000

    #1 add

--- test_script.js
+++ test_script.js
@@ -14,12 +14,12 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * FROM famous_people WHERE first_name = 'Paul' ", (err, result) => {
+  client.query("SELECT * FROM famous_people", (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
     //console.log(result.rows[0].number); //output: 1
-    console.log(result.rows);
+    console.log(result);
     client.end();
   });
 });
